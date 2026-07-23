4bf7585373ea60c35a152455d74e8cfb6806b0ee
jadhavdiksha876-gif <jadhavdiksha876@gmail.com>
2026-07-23 19:51:46 +0530

    Add files via upload

---
+++ practice2.5.py
@@ -0,0 +1,11 @@
+n = int(input("Enter the number:"))
+temp = n
+rev =0
+while(n>0):
+    d=n%10
+    rev=rev*10+d
+    n=n//10
+if(temp==rev):
+    print("The number is palindrome. ")
+else:
+    print("The number is not palindrome.")
