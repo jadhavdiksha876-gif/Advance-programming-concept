956ed1ed1c15f98c5788f9303b48a74013edd63a
jadhavdiksha876-gif <jadhavdiksha876@gmail.com>
2026-07-23 15:58:14 +0530

    Add files via upload

---
+++ practice2.2.py
@@ -0,0 +1,9 @@
+pounds = float(input("Enter weight in pounds: "))
+kg = pounds * 0.453592
+print("Weight in kilograms =", kg)
+
+km = float(input("Enter distance in kilometers: "))
+miles = km * 0.621371
+print("Distance in miles =", miles)
+
+
