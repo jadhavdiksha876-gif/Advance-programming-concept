35007d55cf23c846af47ecf391c2686fe5420781
jadhavdiksha876-gif <jadhavdiksha876@gmail.com>
2026-08-03 21:16:58 +0530

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,120 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+var = 10
+type(var)
+<class 'int'>
+var = 1.1
+type(var)
+<class 'float'>
+var = "Apple"
+type(var)
+<class 'str'>
+var = True
+type(var)
+<class 'bool'>
+var = 5j
+type(var)
+<class 'complex'>
+list =["apple","orange"]
+type(list)
+<class 'list'>
+set = {"apple","banana"}
+type(set)
+<class 'set'>
+dict ={"name":"Diksha","Age":20}
+type(dict)
+<class 'dict'>
+n = None
+type(n)
+<class 'NoneType'>
+a = 4
+b= 3
+a+b
+7
+a-b
+1
+a/b
+1.3333333333333333
+a*b
+12
+a**b
+64
+a//b
+1
+print("Assignment")
+Assignment
+a==b
+False
+a+=b
+5
+5
+b-=a
+b
+-4
+a=6
+a*=b
+a
+-24
+a/=b
+a
+6.0
+a=9
+b=5
+a//=b
+a
+1
+a**=b
+a
+1
+
+a=14
+b=15
+a>b
+False
+a<b
+True
+a<=b
+True
+a>=b
+False
+>>> a==b
+False
+>>> 
+>>> print("logic")
+logic
+>>> a=12
+>>> b=34
+>>> a>10 and b>30
+True
+>>> a<10 or b>30
+True
+>>> not(a=23)
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+>>> not(a==14)
+True
+>>> range(5)
+range(0, 5)
+>>> =45
+SyntaxError: invalid syntax
+>>> a=45
+>>> b=67
+>>> a&b
+1
+>>> x="hello world"
+>>> y="hello"
+>>> x in y
+False
+>>> y in x
+True
+>>> x not in y
+True
+>>> y not in x
+False
+>>> y is x
+False
+>>> x is y
+False
+>>> list =["Diksha",12,67,8.0]
+>>> list.append("hello")
+>>> list
+['Diksha', 12, 67, 8.0, 'hello']
